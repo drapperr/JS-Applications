a69d22fb47db30f2395d3ebdb2f4f475832e8a3a
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-16 17:39:32 +0200

    update

--- ASYNCHRONOUS PROGRAMMING/01.GITHUB-COMMITS/app.js
+++ ASYNCHRONOUS PROGRAMMING/01.GITHUB-COMMITS/app.js
@@ -1,4 +1,31 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const username=document.querySelector('#username').value;
+    const repo=document.querySelector('#repo').value;
+    const ul=document.querySelector('#commits');
+
+    ul.innerHTML='';
+
+    fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+    .then(x=>{
+        if (x.status>=400) {
+           throw x; 
+        }
+        return x.json()
+    })
+    .then(repos=>{
+        repos.forEach(repo => {
+            let author=repo.commit.author.name;
+            let message=repo.commit.message;
+            
+            let newLi=document.createElement('li');
+            newLi.textContent=`${author}: ${message}`;
+
+            ul.appendChild(newLi);
+        });
+    })
+    .catch(e=>{
+        let newLi=document.createElement('li');
+            newLi.textContent=`Error: ${e.status} (${e.statusText})`;
+            ul.appendChild(newLi);
+    })
 }
