1bf56489c4ef67dd80e1020b6868136b16c50c51
DraPpeRr <drapperr@users.noreply.github.com>
2020-03-27 19:18:45 +0200

    update

---
+++ EXERCISE ROUTING/Team-Manager-Skeleton/scripts/requester.js
@@ -0,0 +1,11 @@
+const username='Pesho';
+const password='1234';
+
+function makeHeaders(httpMethod,data){
+    const headers={
+        method: httpMethod,
+        headers:{
+            
+        }
+    }
+}
