4001e393eb41016782b9bdf8e9482fe9fb57d166
DraPpeRr <drapperr@users.noreply.github.com>
2020-05-05 08:10:32 +0300

    update

---
+++ Exams/JS Applications Exam - 16 December 2018/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBZz2-h3r_birnkKM67P8L7A59KplGD9Jw",
+    authDomain: "myexam-05.firebaseapp.com",
+    databaseURL: "https://myexam-05.firebaseio.com",
+    projectId: "myexam-05",
+    storageBucket: "myexam-05.appspot.com",
+    messagingSenderId: "640468188220",
+    appId: "1:640468188220:web:31d53bfcb31abedef91739"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
